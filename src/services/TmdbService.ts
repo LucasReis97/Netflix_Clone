ec5b641c89e599de72c928f207bdacaa70815ece
Lucas Reis <lucas.reguini@gmail.com>
2022-03-02 12:58:43 -0300

    Encapsulation in the informations

--- src/services/TmdbService.ts
+++ src/services/TmdbService.ts
@@ -6,44 +6,156 @@ import SectionModel from '../model/SectionModel'
 import ApiService from './interface/ApiService'
 
 class TmdbService implements ApiService {
-    private API_KEY: string;
-    private API_BASE: string;
+  private API_KEY: string;
+  private API_BASE: string;
 
-    constructor () {
-      this.API_KEY = 'b0ad0ab340f001d21909086b705f4c13'
-      this.API_BASE = 'https://api.themoviedb.org/3'
-    }
+  constructor () {
+    this.API_KEY = process.env.REACT_APP_API_KEY || ''
+    this.API_BASE = process.env.REACT_APP_API_BASE || ''
+    console.log('LOG: ' + this.API_KEY)
+  }
 
-    private async basicFetch (endpoint: string) {
-      const req = await fetch(`${this.API_BASE}${endpoint}`)
-      return await req.json()
-    }
+  private async basicFetch (endpoint: string) {
+    const req = await fetch(`${this.API_BASE}${endpoint}`)
+    return await req.json()
+  }
 
-    public async getHomeList (): Promise<SectionModel[]> {
-      const homeDto: SectionModel[] = []
-      homeDto.push(new SectionModel('originals', 'Originais da Netflix', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/discover/tv?with_network=213&language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      homeDto.push(new SectionModel('trending', 'Recomendados para você', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/trending/all/week?language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      homeDto.push(new SectionModel('toprated', 'Em alta', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/movie/top_rated?language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      homeDto.push(new SectionModel('war', 'Guerra', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/discover/tv?with_genres=10752&language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      homeDto.push(new SectionModel('comedy', 'Comédia', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/discover/tv?with_genres=35&language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      homeDto.push(new SectionModel('family', 'Familia', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/discover/tv?with_genres=10751&language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      homeDto.push(new SectionModel('romance', 'Romance', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/discover/tv?with_genres=10749&language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      homeDto.push(new SectionModel('documentary', 'Documentarios', ObjectMapper.deserializeArray(MovieModel, (await this.basicFetch(`/discover/tv?with_genres=99&language=pt-Br&api_key=${this.API_KEY}`)).results)))
-      return homeDto
-    }
+  public async getHomeList (): Promise<SectionModel[]> {
+    const homeDto: SectionModel[] = []
+    homeDto.push(
+      new SectionModel(
+        'originals',
+        'Originais da Netflix',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/discover/tv?with_network=213&language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    homeDto.push(
+      new SectionModel(
+        'trending',
+        'Recomendados para você',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/trending/all/week?language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    homeDto.push(
+      new SectionModel(
+        'toprated',
+        'Em alta',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/movie/top_rated?language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    homeDto.push(
+      new SectionModel(
+        'war',
+        'Guerra',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/discover/tv?with_genres=10752&language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    homeDto.push(
+      new SectionModel(
+        'comedy',
+        'Comédia',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/discover/tv?with_genres=35&language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    homeDto.push(
+      new SectionModel(
+        'family',
+        'Familia',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/discover/tv?with_genres=10751&language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    homeDto.push(
+      new SectionModel(
+        'romance',
+        'Romance',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/discover/tv?with_genres=10749&language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    homeDto.push(
+      new SectionModel(
+        'documentary',
+        'Documentarios',
+        ObjectMapper.deserializeArray(
+          MovieModel,
+          (
+            await this.basicFetch(
+              `/discover/tv?with_genres=99&language=pt-Br&api_key=${this.API_KEY}`
+            )
+          ).results
+        )
+      )
+    )
+    return homeDto
+  }
 
-    public async getMovieInfo (id:number | undefined, type:string): Promise<IMovieDetailModel> {
-      let info
-      switch (type) {
-        case 'movie':
-          info = await this.basicFetch(`/movie/${id}?language=pt-Br&api_key=${this.API_KEY}`)
-          break
-        case 'tv':
-          info = await this.basicFetch(`/tv/${id}?language=pt-Br&api_key=${this.API_KEY}`)
-          break
-      }
-      return ObjectMapper.deserialize(MovieDetailModel, info)
+  public async getMovieInfo (
+    id: number | undefined,
+    type: string
+  ): Promise<IMovieDetailModel> {
+    let info
+    switch (type) {
+      case 'movie':
+        info = await this.basicFetch(
+          `/movie/${id}?language=pt-Br&api_key=${this.API_KEY}`
+        )
+        break
+      case 'tv':
+        info = await this.basicFetch(
+          `/tv/${id}?language=pt-Br&api_key=${this.API_KEY}`
+        )
+        break
     }
+    return ObjectMapper.deserialize(MovieDetailModel, info)
+  }
 }
 
 export default new TmdbService()
